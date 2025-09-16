a646dabfe7a46fb2f81cdede624b3cbedbb82b2f
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 13:23:23 +0600

    route cleaner updated

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000',         // local dev
-        'http://127.0.0.1:3000',         // local dev alternative
-        'http://192.168.25.202:3000',    // your current frontend IP
-    ],
+    'allowed_origins' => ['http://localhost:3000'],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Authorization'], // if sending JWT
     'supports_credentials' => false,        // JWT does not need cookies
