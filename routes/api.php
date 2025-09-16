5b7bbfb0123a77cf60511214526906f571d6de50
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 16:55:00 +0600

    finalize

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,10 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login',    [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
     Route::get('me',            [AuthController::class, 'me']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('logout',  [AuthController::class, 'logout']);
