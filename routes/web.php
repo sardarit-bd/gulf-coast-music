afc8f95ef98275067eef6341d9550ce530364501
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 13:22:04 +0600

    route cleaner updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/cc', function () {
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear'); // optional, clears compiled views
+    Artisan::call('config:cache'); // optional, rebuild config cache
+
+    return response()->json([
+        'message' => 'All caches cleared successfully!'
+    ]);
 });
