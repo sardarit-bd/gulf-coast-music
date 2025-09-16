c0a0edb31f6db9fc80e0a1a95406fdc07a30f58e
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 13:19:01 +0600

    cors update

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','http://192.168.25.202:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
