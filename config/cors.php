ede681fd0a4e7e0aa2d63bcf3728bcfede3192ff
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 13:20:16 +0600

    cors update

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','http://192.168.25.202:3000'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'http://localhost:3000',         // local dev
+        'http://127.0.0.1:3000',         // local dev alternative
+        'http://192.168.25.202:3000',    // your current frontend IP
+    ],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false, //true korle header a credentials add hobe
+    'exposed_headers' => ['Authorization'], // if sending JWT
+    'supports_credentials' => false,        // JWT does not need cookies
 ];
