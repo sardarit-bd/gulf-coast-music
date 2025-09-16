00e4d67a5573ffaa711805e18a32a3b7c857c11d
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-16 13:29:55 +0600

    route cleaner updated

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,12 @@
 <?php
-
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',               // your local frontend
+        'https://gulf.sardaritskillshare.com', // live backend
+    ],
     'allowed_headers' => ['*'],
-    'exposed_headers' => ['Authorization'], // if sending JWT
-    'supports_credentials' => false,        // JWT does not need cookies
+    'exposed_headers' => ['Authorization'],   // if returning JWT
+    'supports_credentials' => false,         // JWT does not require cookies
 ];
