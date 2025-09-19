de3c606aaf8f201d34987ae09f5428d372197397
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-19 10:31:42 +0600

    user model update

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable implements JWTSubject /*, MustVerifyEmail*/
     public const ROLE_FAN        = 'Fan';
 
     // ---- Fillable ----
+    // ---- Fillable ----
     protected $fillable = [
         'name',
         'email',
