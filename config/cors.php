d825db2a62defa6e85ec52e98196449989f7d069
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-22 10:38:33 +0600

    cors update

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,18 @@
 <?php
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'printify/*', 'login', 'logout', 'me', 'refresh'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://127.0.0.1:3000',
         'https://gulf-coast.vercel.app',
-        'https://vercel.com/',
-        'http://127.0.0.1:3000',       // your local frontend
-        'https://gulf.sardaritskillshare.com', // live backend
     ],
+
     'allowed_headers' => ['*'],
-    'exposed_headers' => ['Authorization'],   // if returning JWT
-    'supports_credentials' => false,         // JWT does not require cookies
+
+    'exposed_headers' => ['Authorization'],
+
+    'supports_credentials' => false, // JWT = false is fine
 ];
