842f9cec42ac2c789c2dc868e962ba7420d3ddd3
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-28 12:29:44 +0600

    Artist photo controller update

--- app/Http/Controllers/Artist/ArtistPhotoController.php
+++ app/Http/Controllers/Artist/ArtistPhotoController.php
@@ -27,7 +27,7 @@ class ArtistPhotoController extends Controller
 
             return response()->json([
                 'data' => [
-                    'photos' => url('public/'.$artist->image),
+                    'photos' => url('public/'.$photos),
                 ],
                 'success' => true,
                 'status' => 200,
